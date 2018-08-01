0136213e690784c163f55b4a12971f7fceb4e2c1
mr.dzoviii <joco-95@hotmail.com>
2018-08-01 11:10:58 +0200

    Travel advertiser sredjen

--- TravelAdvertiser/WebContent/resources/js/script.js
+++ TravelAdvertiser/WebContent/resources/js/script.js
@@ -1,1 +1,13 @@
-
+function toogleForm(){
+	var selector=document.getElementById("ad_type").value;
+	var paid=document.getElementById("ad_paid");
+	var free=document.getElementById("ad_free");
+	if(selector=="Free"){
+		paid.style.display="none";
+		free.style.display="block";
+	}
+	else{
+		paid.style.display="block";
+		free.style.display="none";
+	}
+}
